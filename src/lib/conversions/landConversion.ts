5cec53c4cf15e54a366bfd199c44e8660726708a
shoieb-alam <shoieb.ctg@gmail.com>
2025-11-20 10:13:54 +0600

    fix: correct total Gonda calculation in shotokToKaniGonda function

--- src/lib/conversions/landConversion.ts
+++ src/lib/conversions/landConversion.ts
@@ -3,7 +3,7 @@ export const shotokToSqFeet = (shotok: number): number => shotok * 435.6;
 export const shotokToKatha = (shotok: number): number => (shotok * 435.6) / 721.46;
 
 export const shotokToKaniGonda = (shotok: number) => {
-  const totalGonda = shotok / 1.9835;
+  const totalGonda = shotok / 2;
   const kani = Math.floor(totalGonda / 20);
   const remainingGonda = totalGonda % 20;
   const gonda = Math.floor(remainingGonda);
